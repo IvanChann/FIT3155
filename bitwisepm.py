6bc5998633caf0f64c49068a713181a79607e63d
ivan <icha0015@student.monash.edu>
2023-08-19 17:34:56 +1000

    edited delta to be in place

--- bitwisepm.py
+++ bitwisepm.py
@@ -3,13 +3,16 @@ from bitarray import bitarray
 def bitwisepm(text: str, pat: str):
     m = len(pat)
     bitv = bitarray("1" * m)
+    delta = bitarray("0"  * m)
+    
     result = []
     
     for j in range(len(text)):
-        delta = bitarray(1 if text[j] != pat[i] else 0 for i in range(m - 1, -1, -1))
+        for i in range(m - 1, -1, -1):
+            delta[m - i - 1] = 1 if text[j] != pat[i] else 0
         bitv = (bitv << 1) | delta
         if not bitv[0]:
             result.append(j - m + 1)
     return result
 
-print(bitwisepm("abcab" , "abcabcabc"))
+print(bitwisepm("xaadadadaxdad" , "dadax"))
