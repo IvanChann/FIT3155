90a8b1b2d652130df4e92b202c86c8f6f3e5dc5c
ivan <icha0015@student.monash.edu>
2023-10-23 22:10:02 +1100

    finished q1 i tihnk

--- ptimesq.py
+++ ptimesq.py
@@ -1,78 +1,110 @@
 import random
+import sys
 
-def miller_rabin_test(n, k=5):
-    if n < 2:
-        return False
-    if n in [2, 3]:
-        return True
-    if n % 2 == 0:
-        return False
+def gen_n_bit_random_number(n):
+    # OR (1 << (n-1)) ensures MSB is 1 so size is guaranteed n bits
+    # OR 1 guarantees it odd (LSB = 1)
+    return random.getrandbits(n) | (1 << (n-1)) | 1
 
-    r, s = 0, n - 1
-    while s % 2 == 0:
-        r += 1
-        s //= 2
-
-    for _ in range(k):
-        a = random.randint(2, n - 1)
-        x = pow(a, s, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
-
-def karatsuba_multiply(x, y):
-    if x < 10 or y < 10:
-        return x * y
-    
-    n = max(len(str(x)), len(str(y)))
-    m = n // 2
-
-    a, b = divmod(x, 10**m)
-    c, d = divmod(y, 10**m)
-
-    ac = karatsuba_multiply(a, c)
-    bd = karatsuba_multiply(b, d)
-    ad_plus_bc = karatsuba_multiply(a+b, c+d) - ac - bd
-
-    return ac * 10**(2*m) + ad_plus_bc * 10**m + bd
-
-def generate_n_bit_random_number(n):
-    return random.randint(2**(n-1), 2**n - 1)
-
-def generate_n_bit_prime(n):
+def gen_n_bit_prime(n):
     while True:
-        candidate = generate_n_bit_random_number(n)
+        candidate = gen_n_bit_random_number(n)
         if miller_rabin_test(candidate):
             return candidate
 
-if __name__ == "__main__":
-    import sys
+def modular_exponentiation(a, b, n):
+    binary_rep = bin(b)[2:] # convert b to its binary representation
+    
+    # Base case
+    current = a % n
+    if binary_rep[-1] == "1" :  # last element is LSB
+        result = current
+    else:
+        result = 1
 
-    if len(sys.argv) != 2:
-        print("Usage: python ptimesq.py <n>")
-        sys.exit(1)
+    # Iterating through remaining bits in the binary representation in reverse
+    for current_bit in reversed(binary_rep[:-1]):
+        # Compute the next term in the sequence
+        current = (current * current) % n
+        if current_bit == "1":
+            # update result
+            result = (result * current) % n
 
+    return result
+
+
+# function for miller rabin test
+# returns true for probably prime, false for composite
+def miller_rabin_test(n, k=5):
+    # special cases
+    if n == 2 or n == 3:
+        return True
+    # only testing odd integers (can technically skip as gen_n_bit_random_number only generates odd numbers)
+    if n % 2 == 0:
+        return False
+    # Factor n-1 as (2^s)*t, where t is odd
+    s = 0
+    t = n - 1
+    while t % 2 == 0:
+        s += 1
+        t //= 2
+
+    # running k random tests
+    for _ in range(k):
+        # selecting random witness
+        a = random.randint(2, n - 2)
+
+        # Compute the sequence [x_0, x_1, ... , x_s] with modular exponentiation by repeated squaring
+        x_values = [modular_exponentiation(a, t, n)] # modular expontentiation to calculate a ^ t mod n
+        for _ in range(s):
+            x_values.append((x_values[-1] * x_values[-1]) % n)
+
+        # Check if n satisfies Fermat’s little theorem for this witness
+        if x_values[-1] != 1:
+            return False
+
+        # Run the sequence test
+        for j in range(1, s + 1):
+            if x_values[j] == 1 and (x_values[j - 1] != 1 and x_values[j - 1] != n - 1):
+                return False
+
+    # n has passed all tests so probably prime
+    return True
+
+
+# recursive implementation of karatsuba multiplication in base 10
+def karatsuba_multiply(u, v):
+    # base case
+    if u < 10 or v < 10:
+        return u * v
+    
+    n = max(len(str(u)), len(str(v))) # getting max digits
+    m = n // 2
+
+    # splitting u and v, divmod returns quotient and remainder
+    u_1, u_0 = divmod(u, 10**m) 
+    v_1, v_0 = divmod(v, 10**m)
+
+    return (10**(2*m) + 10**m) * karatsuba_multiply(u_1, v_1) - 10**m * karatsuba_multiply(u_1 - u_0, v_1 - v_0) + (10**m + 1) * karatsuba_multiply(u_0, v_0)
+
+        
+
+if __name__ == "__main__":   
     n = int(sys.argv[1])
-
+    
     if not (32 <= n <= 2046):
-        print("n should be between 32 and 2046.")
+        print("n should be between 32 and 2046")
         sys.exit(1)
 
-    p, q = generate_n_bit_prime(n), generate_n_bit_prime(n)
+    p, q = gen_n_bit_prime(n), gen_n_bit_prime(n)
     while p == q:
-        q = generate_n_bit_prime(n)
+        q = gen_n_bit_prime(n)
 
-    product = karatsuba_multiply(p, q)
+    product = karatsuba_multiply(p, q)  
 
-    with open('output_ptimesq.txt', 'w') as f:
-        f.write(f"{p}\n")
-        f.write(f"{q}\n")
-        f.write(f"{product}\n")
+    with open('output_ptimesq.txt', 'w') as file:
+        file.write(f"#p (in base 10)]\n{p}\n\n")
+        file.write(f"#q (in base 10)\n{q}\n\n")
+        file.write(f"p*q (in base 10)\n{product}\n")
 
     print(f"Results written to output_ptimesq.txt")
